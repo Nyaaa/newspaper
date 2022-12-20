ee24191cd4eded5b67aeb0edb00c89abb98997c6
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2022-12-20 22:55:21 +0300

    populating tables

--- commands.py
+++ commands.py
@@ -1,6 +1,5 @@
 print('imports...')
 from news.models import *
-import random
 
 print('creating users...')
 u1 = User.objects.create_user('user01')
@@ -10,19 +9,19 @@ a1 = Author.objects.create(user=u1)
 a2 = Author.objects.create(user=u2)
 
 print('creating data...')
-c1 = Category.objects.create(name='Facepalm')
-c2 = Category.objects.create(name='Awww')
-c3 = Category.objects.create(name='LOL')
-c4 = Category.objects.create(name='Nope')
+ct1 = Category.objects.create(name='Facepalm')
+ct2 = Category.objects.create(name='Awww')
+ct3 = Category.objects.create(name='LOL')
+ct4 = Category.objects.create(name='Nope')
 
-p1 = Post.objects.create(author=a1, type='a', title='Test article #1', text="Lorem ipsum")
-p2 = Post.objects.create(author=a2, type='a', title='Test article #2', text="Lorem ipsum")
-p3 = Post.objects.create(author=a1, type='n', title='Test news piece #1', text="Lorem ipsum")
+p1 = Post.objects.create(author=a1, type='a', title='Test article #1', text='Suspendisse tristique est faucibus magna commodo condimentum. Duis sodales enim vel neque scelerisque volutpat. Nunc in ligula purus. Curabitur molestie arcu vel ultricies mattis. Proin dolor ex, ultricies ac ullamcorper sit amet, malesuada eu purus. Cras nec dapibus eros. Aenean volutpat augue ante. Sed vulputate varius pulvinar. Ut sed tempus augue. Praesent accumsan urna tortor. Nunc porttitor nisl leo. Donec aliquam nisl eget velit egestas, in tempor nunc tempus. Donec feugiat malesuada bibendum.')
+p2 = Post.objects.create(author=a2, type='a', title='Test article #2', text='Donec pulvinar purus odio, non placerat nisi volutpat ac. Duis in varius magna. Phasellus nec sagittis eros, non accumsan est. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin suscipit tempor libero non mattis. Donec consequat sagittis est in laoreet. Sed sagittis nibh sem, eu tempor massa sollicitudin sit amet. Morbi posuere arcu ac metus laoreet efficitur.')
+p3 = Post.objects.create(author=a1, type='n', title='Test news piece #1', text='Nunc egestas purus ut enim tincidunt, a ultricies arcu condimentum. Mauris vel suscipit nibh. Nam dui ligula, auctor et finibus quis, rhoncus in urna. Vestibulum consectetur tellus suscipit, volutpat massa quis, bibendum ante. Nunc ultricies sed est sed tincidunt. Cras nec velit in erat ultricies facilisis. In dolor lorem, lobortis vitae turpis et, fermentum lacinia sapien. Suspendisse justo massa, suscipit at quam at, elementum aliquam turpis. Pellentesque et vestibulum justo, eget dapibus lacus. Sed vitae tempus urna, sed convallis dolor.')
 
-PostCategory.objects.create(post=p1, category=c1)
-PostCategory.objects.create(post=p2, category=c2)
-PostCategory.objects.create(post=p3, category=c3)
-PostCategory.objects.create(post=p3, category=c4)
+PostCategory.objects.create(post=p1, category=ct1)
+PostCategory.objects.create(post=p2, category=ct2)
+PostCategory.objects.create(post=p3, category=ct3)
+PostCategory.objects.create(post=p3, category=ct4)
 
 c1 = Comment.objects.create(post=p1, user=u1, text='comment01')
 c2 = Comment.objects.create(post=p2, user=u1, text='comment02')
@@ -30,23 +29,22 @@ c3 = Comment.objects.create(post=p3, user=u2, text='comment03')
 c4 = Comment.objects.create(post=p1, user=u1, text='comment04')
 c5 = Comment.objects.create(post=p2, user=u2, text='comment05')
 
-obj = [p1, p2, p3, c1, c2, c3, c4, c5]
+p1.like()
+p2.like()
+p2.like()
+p3.like()
+c1.dislike()
+c2.like()
+c3.like()
+c4.like()
+c5.dislike()
 
-for i in obj:
-    c = bool(random.getrandbits(1))
-    i.like() if c else i.dislike()
-
-print('top ratings:')
 a1.update_rating()
 a2.update_rating()
 
-print('top author:')
-Author.objects.all().order_by('_rating').values('user__username', '_rating').last()
-
-print('top article:')
-Post.objects.filter(type='a').order_by('rating').values('created', 'author__user__username', 'rating', 'title').last()
-p = Post.objects.filter(type='a').order_by('rating').last()
-print('preview:')
-p.preview()
-print('comments:')
-Comment.objects.filter(post=p).values('created', 'user', 'rating', 'text')
+print('Top author:', Author.objects.all().order_by('_rating').values('user__username', '_rating').last())
+print('Top article:', Post.objects.filter(type='a').order_by('rating').values('created', 'author__user__username', 'rating', 'title').last())
+top_post = Post.objects.filter(type='a').order_by('rating').last()
+print('Text preview:', top_post.preview())
+print('Comments:', Comment.objects.filter(post=top_post).values('created', 'user__username', 'rating', 'text'))
+print('Done')
--- newspaper/news/models.py
+++ newspaper/news/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Sum
+
 
 # Create your models here.
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    _rating = models.IntegerField(default = 0, db_column = 'rating')
+    _rating = models.IntegerField(default=0, db_column='rating')
 
     @property
     def rating(self):
@@ -12,30 +14,27 @@ class Author(models.Model):
 
     def update_rating(self):
         self._rating = 0
-        # posts by Author
-        for p in Post.objects.filter(author = self):
-            self._rating += p.rating * 3
-            print(p, p.rating)
-            # comments to posts by Author
-            for comm in Comment.objects.filter(post = p):
-                self._rating += comm.rating
-                print(comm, comm.rating)
 
+        # posts by Author
+        self._rating += Post.objects.filter(author=self).aggregate(Sum('rating'))['rating__sum'] * 3
+        # comments to posts by Author
+        self._rating += Comment.objects.filter(post__author=self).aggregate(Sum('rating'))['rating__sum']
         # comments by Author
-        for c in Comment.objects.filter(user = self.user):
-            self._rating += c.rating
-            print(c, c.rating)
+        self._rating += Comment.objects.filter(user=self.user).aggregate(Sum('rating'))['rating__sum']
 
         self.save()
 
+
 class Category(models.Model):
-    name = models.CharField(max_length = 255, unique = True)
+    name = models.CharField(max_length=255, unique=True)
+
 
 class PostCategory(models.Model):
-    post = models.ForeignKey('Post', on_delete = models.CASCADE)
-    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
-news ='n'
+
+news = 'n'
 article = 'a'
 
 POST_TYPES = [
@@ -43,11 +42,12 @@ POST_TYPES = [
     (article, 'Article')
 ]
 
+
 class Post(models.Model):
-    author = models.ForeignKey(Author, on_delete = models.CASCADE)
-    type = models.CharField(max_length= 1, choices = POST_TYPES, default=news)
-    created = models.DateTimeField(auto_now_add = True)
-    title = models.CharField(max_length = 255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    type = models.CharField(max_length=1, choices=POST_TYPES, default=news)
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
     text = models.TextField()
     rating = models.IntegerField(default=0)
 
@@ -62,10 +62,11 @@ class Post(models.Model):
     def preview(self):
         return self.text[0:124] + '...'
 
+
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete = models.CASCADE)
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
-    created = models.DateTimeField(auto_now_add = True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
     text = models.TextField()
     rating = models.IntegerField(default=0)
 
