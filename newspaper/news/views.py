f4e6150048e0bfd96143a6a45f25fe9c02f7a0e6
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2022-12-27 00:26:39 +0300

    templates

--- newspaper/news/views.py
+++ newspaper/news/views.py
@@ -1,12 +1,18 @@
 from django.views.generic import ListView, DetailView
 from .models import Post
+from datetime import datetime
 
 # Create your views here.
 class PostList(ListView):
     model = Post
-    ordering = 'created'
+    ordering = '-created'
     template_name = 'posts.html'
     context_object_name = 'posts'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        return context
 
 class PostDetail(DetailView):
     model = Post
