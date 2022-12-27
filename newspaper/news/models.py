05e832bd12efadfadb8d8264701a0b32a79b0b12
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2022-12-27 17:02:46 +0300

    categories

--- newspaper/news/models.py
+++ newspaper/news/models.py
@@ -30,6 +30,9 @@ class Author(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
+    def __str__(self):
+        return f'{self.name}'
+
 
 class PostCategory(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
